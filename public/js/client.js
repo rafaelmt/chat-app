4a4bb674035e7f2abcff0cc4b6eb5652b8b913e4
Rafael Teixeira <rafaelmedtex@gmail.com>
2016-03-10 16:25:41 -0300

    Send messages, display received messages

--- public/js/client.js
+++ public/js/client.js
@@ -1,20 +1,39 @@
+var ws;
+
 function connect(userName) {
 	//TODO: url-encode
 	var server = 'wss://codingtest.meedoc.com/ws?username=' + userName;
-	var ws = new WebSocket(server);
+	ws = new WebSocket(server);
 	ws.onopen = function() {
 		console.log("connected as " + userName);
+		$('#login').slideUp();
 	};
 	ws.onmessage = function(event) {
 		//TODO: check if JSON is valid
 		var data = $.parseJSON(event.data);
-		$( "#messages" ).append( "<p>" + data.message + "</p>" );
+		appendMessage(data.message);
 	};
+};
+
+function appendMessage(message) {
+	$( "#messages" ).append( "<p>" + message + "</p>" );
+}
+
+function send(message) {
+	//TODO: check if connected
+	ws.send(message);
 };
 
 $(document).ready(function() {
 	$('#btnConnect').on('click', function() {
 		var userName = $('#userName').val();
 		connect(userName);
+	});
+
+	$('#btnSend').on('click', function() {
+		var message = $('#messageInput').val();
+		$('#messageInput').val('');
+		appendMessage(message);
+		send(message);
 	});
 });
