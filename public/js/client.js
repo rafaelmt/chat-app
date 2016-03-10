88b0fd09e6bd990aaae1628fea0d761c9d1cc567
Rafael Teixeira <rafaelmedtex@gmail.com>
2016-03-10 15:12:37 -0300

    Getting user name to connect to chat server

--- public/js/client.js
+++ public/js/client.js
@@ -1,9 +1,15 @@
-function connect() {
-	var userName = "test";
+function connect(userName) {
 	//TODO: url-encode
 	var server = 'wss://codingtest.meedoc.com/ws?username=' + userName;
 	var ws = new WebSocket(server);
 	ws.onopen = function() {
-		console.log("yay");
+		console.log("connected as " + userName);
 	};
-}
+};
+
+$(document).ready(function() {
+	$('#btnConnect').on('click', function() {
+		var userName = $('#userName').val();
+		connect(userName);
+	});
+});
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app 		= express();
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
+app.use('/node_modules',  express.static(__dirname + '/node_modules'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/public/views/index.html"); 
