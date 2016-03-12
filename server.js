ef778346383fa8e6f151f106b98adedf4fcbe22b
Rafael Teixeira <rafaelmedtex@gmail.com>
2016-03-12 17:35:50 -0300

    Add GET call on server to provide avatars

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express 	= require('express');
 var WebSocket 	= require('ws');
 var morgan 		= require('morgan');
+var rest 		= require('restler');
 var app 		= express();
 
 app.use(morgan('dev'));
@@ -11,6 +12,42 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + "/public/views/index.html"); 
 });
 
+app.get('/avatar', function (req, res) {
+	if(req.query) {
+		getAvatar(req.query.name, function(imageUrl){
+			if(imageUrl) {
+				res.setHeader('content-type', 'application/json');
+				res.send({url: imageUrl});
+			} else {
+				res.send();
+			}
+		});
+	} else {
+		res.send();
+	}
+});
+
 app.listen(3000, function () {
   console.log('MeeChat listening on port 3000');
 });	
+
+
+function getAvatar(name, callback) {
+	var encodedName = encodeURIComponent(name);
+	var searchUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=' + encodedName;
+	rest.get(searchUrl).on('complete', function(data) {
+		var url = parseResponse(data);
+		callback(url);
+	});
+}
+
+function parseResponse(jsonObj) {
+	var imageUrl;
+	if(jsonObj.query) {
+		var pageId = Object.keys(jsonObj.query.pages);
+		if(pageId && jsonObj.query.pages[pageId].thumbnail) {
+			imageUrl = jsonObj.query.pages[pageId].thumbnail.original;
+		}
+	}
+	return imageUrl;
+}
